ee667c161d8f943e491853fcfeeac4ae9074c391
Aditya <adithebest.ar@gmail.com>
2021-07-31 21:52:21 +0530

    Added comments for more readability

--- Week01/Mod04-numbersgame.cpp
+++ Week01/Mod04-numbersgame.cpp
@@ -14,6 +14,9 @@ const int MOD = 1e9 + 7;
 const int inf = 1e9 + 10;
 const long long INF = 1e18 + 10;
 
+// Do not use an approximation - it won't be good enough
+// with the large numbers involved. 
+
 const double golden = (1 + sqrt(5)) / 2;
 
 int main () {
@@ -24,10 +27,21 @@ int main () {
         cin >> a1 >> a2 >> b1 >> b2;
         long long ans = 0;
 
+        // Remember to account for symmetry when working through the
+        // conditionals and counting in the code below.
+
         for (int b = b1; b <= b2; b++) {
             if (floor(golden * b) < a1 || floor((golden - 1) * b) > a2)
+                
+                //# When either the lower threshold is below a1 or the upper threshold is above a2,
+                // all games are winning
+
                 ans += (a2 - a1 + 1);
             else {
+
+                //# When the thresholds intercept the (a1,a2) interval,
+                // we have to explicitly count the # of winning games.
+
                 ans += max(0.0, a2 - floor(golden * b));
                 ans += max(0.0, ceil((golden - 1) * b) - a1);
             }
