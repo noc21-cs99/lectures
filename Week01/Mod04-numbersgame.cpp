a416341671b0738e8342ef4001f24521899f1523
Aditya <adithebest.ar@gmail.com>
2021-07-31 19:22:45 +0530

    Added Mod04-numbersgame.cpp

---
+++ Week01/Mod04-numbersgame.cpp
@@ -0,0 +1,41 @@
+//Problem Link : https://codingcompetitions.withgoogle.com/codejam/round/0000000000433085/0000000000432fe1
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define trav(a,x) for (auto& a : x)
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+const int MOD = 1e9 + 7;
+const int inf = 1e9 + 10;
+const long long INF = 1e18 + 10;
+
+const double golden = (1 + sqrt(5)) / 2;
+
+int main () {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int tc = 1; cin >> tc;
+    for (int i = 1; i <= tc; i++) {
+        double a1, a2, b1, b2;
+        cin >> a1 >> a2 >> b1 >> b2;
+        long long ans = 0;
+
+        for (int b = b1; b <= b2; b++) {
+            if (floor(golden * b) < a1 || floor((golden - 1) * b) > a2)
+                ans += (a2 - a1 + 1);
+            else {
+                ans += max(0.0, a2 - floor(golden * b));
+                ans += max(0.0, ceil((golden - 1) * b) - a1);
+            }
+        } 
+
+
+        cout << "Case #" << i << ":" << ' ' << ans << '\n';
+    }
+}
+
+//Know more about Golden ratio : https://en.wikipedia.org/wiki/Golden_ratio
