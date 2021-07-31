53d09ddd092bab08a5bc61556026cb813c0f03d7
Aditya <adithebest.ar@gmail.com>
2021-07-31 21:54:54 +0530

    Solution for Mod03 in C++

--- Week01/Mod04-numbersgame.cpp
+++ Week01/Mod04-numbersgame.cpp
@@ -33,13 +33,13 @@ int main () {
         for (int b = b1; b <= b2; b++) {
             if (floor(golden * b) < a1 || floor((golden - 1) * b) > a2)
                 
-                //# When either the lower threshold is below a1 or the upper threshold is above a2,
+                // When either the lower threshold is below a1 or the upper threshold is above a2,
                 // all games are winning
 
                 ans += (a2 - a1 + 1);
             else {
 
-                //# When the thresholds intercept the (a1,a2) interval,
+                // When the thresholds intercept the (a1,a2) interval,
                 // we have to explicitly count the # of winning games.
 
                 ans += max(0.0, a2 - floor(golden * b));
